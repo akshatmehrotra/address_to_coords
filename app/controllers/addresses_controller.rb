0a4c1a2a86bc1ad673e911f1ab023ff5ec1ce9b7
akshatmehrotra <akshatmehrotra@gmail.com>
2014-05-03 22:50:06 -0500

    added a form to get address from user

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,14 +3,19 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
-    @address = "the corner of Foster and Sheridan"
-    @url_safe_address = URI.encode(@address)
+    # @address = "the corner of Foster and Sheridan"
+
+    @url_safe_address = URI.encode(params[:user_address])
 
     # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @latitude = ?
-    # @longitude = ?
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address="+@url_safe_address+"&sensor=false"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+
+    first = parsed_data["results"][0]
+    geometry=first["geometry"]
+    location = geometry["location"]
+    @latitude = location["lat"]
+    @longitude = location["lng"]
   end
 end
